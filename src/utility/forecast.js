e44bbbe1ee6e8683df0fbe29ed9dacf637ece06b
avnish kumar <avnishkumar@avnishs-MacBook-Pro.local>
2021-02-10 16:03:32 -0500

    temp format changes

--- src/utility/forecast.js
+++ src/utility/forecast.js
@@ -1,13 +1,13 @@
 const request = require('request')
 const forecast = (lat,long,callback)=>{
-    const uri = 'http://api.weatherstack.com/current?access_key=17e5c83c2bd62d6f3bcc121e0fdd76a6&query='+lat+ ','+long+'&units=f'
+    const uri = 'http://api.weatherstack.com/current?access_key=17e5c83c2bd62d6f3bcc121e0fdd76a6&query='+lat+ ','+long+'&units=m'
     request({url:uri,json:true},(error,response)=>{
         if(error){
             callback('URL is not reachable',undefined)
         }else if(response.body.error){
             callback('API request failed',undefined)
         }else{
-            callback(undefined,`It is cuurently ${response.body.current.temperature} degree out. There is chance of ${response.body.current.weather_descriptions}`)
+            callback(undefined,`${response.body.current.temperature + '°C'}, ${response.body.current.weather_descriptions}`)
         }
     })
 }
